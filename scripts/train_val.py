71a5c89ca2898253f7176f8090b1111fc61f396e
xingdongdong <xingdongdong007@163.com>
2018-01-08 16:18:24 +0800

    add train_val.py
    Signed-off-by: xingdongdong <xingdongdong007@163.com>

---
+++ scripts/train_val.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+from os import walk
+from os.path import join
+import os
+
+classCount = 0
+labels = []
+filesList = []
+X0 = []
+y = []
+k = 0
+
+
+root_path = "../images/"
+#"""
+for x in walk(join(root_path, "Train/")):
+    filescount = 0
+    fc = 0
+    if x[0].__contains__("missclassified"):
+        continue
+    if len(x[2]) > 0:
+        print "Processing class %i..."% (k)
+        k += 1
+        for filename in x[2]:
+            if filename.endswith(".jpg") or filename.endswith(".JPG") or filename.endswith(".JPEG") or filename.endswith(".jpeg"):
+                filesList.append(str(x[0]) + "/" + filename + " " + str(classCount))
+                #print join(x[0], filename)
+                filescount += 1
+        if filescount > 0:
+            labels.append(str(x[0]) + " " + str(classCount))
+            classCount += 1
+        else:
+            continue
+        print classCount-1, filescount
+
+models_root = root_path
+print 'Creating train labels'
+f = open(join(models_root, "train.txt"), 'wb')
+for lbl in filesList:
+    f.write(lbl + '\n')
+f.close()
+
+k = 0
+X1 = []
+y1 = []
+classCount = 0
+labels = []
+filesList = []
+for x in walk(join(root_path, "Val/")):
+    filescount = 0
+    fc = 0
+    if x[0].__contains__("missclassified"):
+        continue
+    if len(x[2]) > 0:
+        print "Processing class %i..."% (k)
+        k += 1
+        for filename in x[2]:
+            if filename.endswith(".jpg") or filename.endswith(".JPG") or filename.endswith(".JPEG") or filename.endswith(".jpeg"):
+                filesList.append(str(x[0]) + "/" + filename + " " + str(classCount))
+                #print join(x[0], filename)
+                filescount += 1
+        if filescount > 0:
+            labels.append(str(x[1]) + " " + str(classCount))
+            classCount += 1
+        else:
+            continue
+        print classCount-1, filescount
+
+models_root = root_path
+print 'Creating test labels'
+f = open(join(models_root, "val.txt"), 'wb')
+for lbl in filesList:
+    f.write(lbl + '\n')
